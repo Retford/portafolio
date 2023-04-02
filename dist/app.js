56519ca360de738ee2269d476cff08abf0462306
Retford <retfordjb@gmail.com>
2023-04-02 15:44:28 -0500

    End

---
+++ dist/app.js
@@ -0,0 +1,88 @@
+const menuButton = document.querySelector('#menu-button');
+const menuItems = document.querySelector('#menu');
+const menuButtonSpans = document.querySelectorAll('#menu-button span');
+const links = document.querySelectorAll('#menu a');
+
+// ABRIR MENU
+
+menuButton.addEventListener('click', () => {
+  menuItems.classList.toggle('hidden');
+  menuButtonSpans.forEach((span) => {
+    span.classList.toggle('animado');
+  });
+});
+
+links.forEach((link) => {
+  link.addEventListener('click', () => {
+    menuItems.classList.add('hidden');
+    menuButtonSpans.forEach((span) => {
+      span.classList.remove('animado');
+    });
+  });
+});
+
+// Cierra cuando maximizamos
+
+window.onresize = () => {
+  let w = window.outerWidth;
+  if (w > 768) {
+    menuItems.classList.add('hidden');
+    menuButtonSpans.forEach((span) => {
+      span.classList.remove('animado');
+    });
+  }
+};
+
+// DARK MODE
+
+const darkButton = document.querySelector('#darkButton');
+
+darkButton.addEventListener('click', () => {
+  if (localStorage.getItem('color-theme')) {
+    if (localStorage.getItem('color-theme') === 'light') {
+      document.documentElement.classList.add('dark');
+      localStorage.setItem('color-theme', 'dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+      localStorage.setItem('color-theme', 'light');
+    }
+  } else {
+    if (document.documentElement.classList.contains('dark')) {
+      document.documentElement.classList.remove('dark');
+      localStorage.setItem('color-theme', 'light');
+    } else {
+      document.documentElement.classList.add('dark');
+      localStorage.setItem('color-theme', 'dark');
+    }
+  }
+});
+
+// VARIABLES DE JOB DESCRIPCION
+
+const jobSection = document.querySelector('#jobSection');
+const articles = document.querySelectorAll('#jobSection article');
+
+jobSection.addEventListener('click', (e) => {
+  const id = e.target.dataset.id;
+  console.log(id);
+  if (id) {
+    articles.forEach((article) => {
+      article.classList.add('hidden');
+    });
+    const element = document.getElementById(id);
+    element.classList.remove('hidden');
+  }
+});
+
+const jobLinks = document.querySelectorAll('#jobSection button');
+const firstLink = document.querySelector('#link1');
+
+jobLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    firstLink.classList.remove(
+      'border-sky-500',
+      'bg-white',
+      'dark:bg-slate-700'
+    );
+  });
+});
